0544061b4f95bf7e8e0a29ccecabe30ba603edcc
Aleraisa <alexandra.alvarado1@unmsm.edu.pe>
2024-07-15 12:51:49 -0500

    final version, frontend updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Chat from './components/ChatComponent';
 import RecommendationForm from './components/RecommendationForm';
 import './styles/App.css';
 
